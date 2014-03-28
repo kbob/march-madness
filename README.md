f5cf01c5065957212ac87bfb76ab5600c218e2d5
kbob <kbob@Jogger-egg.com>
2014-03-28 10:09:38 -0700

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,78 @@
+# March Madness 2013
+
+A quick and dirty Python script to analyze people's progress on the
+March Madness pool.
+
+The script calculates and prints each player's minimum and maximum
+possible scores, based on the games that have been played.
+
+
+## Recording a Game
+
+Edit the global variable **actual** and change None to either 0 or 1.
+
+
+## Adding a Player
+
+Edit the global variable predictions and add a player and her predictions.
+
+To check your data entry, call `print_player` with your new player's name, run
+the script, and check the data.
+
+
+## Data
+
+These are the global variables.
+
+ - **scores** - how many points are awarded per game in each round.
+ 
+ - **teams** - the names of the teams.  Feel free to change the spelling
+    on these.
+    
+ - **width** - the width of the longest team's name
+ 
+ - **predictions** - maps player names to their predictions.  See
+   _Prediction Data Format_ below.
+   
+ - **p_width** - the width of the longest player's name   
+ 
+ - **actual** - the actual winners of the games played so far.
+   The format is the same as for predictions, except that the value
+   `None` is used for games that have not yet been played.
+
+
+## Prediction Data Format
+ 
+Each prediction set is a list of lists of Booleans.  Each sublist
+represents one round of the playoffs, so there are six inner lists,
+and they have length  32, 16, 8, 4, 2, and 1.
+ 
+Each game's prediction is recorded as 0 or 1.  0 means the team
+listed formerly will win, and 1 means the latter team will win.
+ 
+For example, in Round 1, I predicted that VCU would beat
+SF Austin.  That games is in the first round (sublist 0), third game
+(index 2), and VCU is listed before Austin, so I put a zero for that space.
+
+    predictions['Bob'][0][2] = 0
+    
+The numbers are always 0 and 1.  The actual team names depend on which
+teams were predicted for previous rounds.
+
+The data is actually entered as a big map literal.
+
+    predictions = {
+        'Bob':                # player name
+             [
+              [0, 1, 0, ...], # 1st round: Fla, Pitt, VCU, ... (32 games)
+              [0, 1, 1, ...], # 2nd round: Fla, UCLA, Syr, ... (16 games)
+              [0, 1, 1, ...], # 3rd round: 8 games
+              [0, 0, 0, 0,]   # 4th round: 4 games
+              [0, 1],         # 5th round: 2 games
+              [1]             # final game
+             ],
+
+    # ... other players
+    }
+ 
+ 
---
+++ march-madness.py
@@ -0,0 +1,206 @@
+scores = [1, 3, 6, 10, 15, 25]
+
+teams = '''
+Florida
+Albany/MSM
+Colorado
+Pittsburgh
+VCU
+SF Austin
+UCLA
+Tulsa
+Ohio St.
+Dayton
+Syracuse
+W. Michigan
+New Mexico
+Stanford
+Kansas
+E. Kentucky
+
+Virginia
+Coastal Carolina
+Memphis
+George Washington
+Cincinnati
+Harvard
+Michigan St.
+Delaware
+North Carolina
+Providence
+Iowa St.
+N.C. Central
+Connecticut
+St. Joseph\'s
+Villanova
+Milwaukee
+
+Arizona
+Weber St.
+Gonzaga
+Oklahoma St.
+Oklahoma
+N. Dakota St.
+San Diego St.
+New Mexico St.
+Baylor
+Nebraska
+Creighton
+La-Lafayette
+Oregon
+BYU
+Wisconsin
+American
+
+Wichita St.
+Cal Poly/Texas So
+Kentucky
+Kansas St.
+St. Louis
+NC State/Xavier
+Louisville
+Manhattan
+UMass
+Iowa/Tennessee
+Duke
+Mercer
+Texas
+Arizona St.
+Michigan
+Wofford
+'''
+teams = [t.strip() for t in teams.split('\n') if t.strip()]
+assert len(teams) == 64
+width = len(max(teams, key=len))
+
+predictions = {
+    'Ali':    [[0, 1, 0, 0,  0, 0, 1, 0,   0, 1, 0, 0,  0, 0, 1, 0,
+                0, 1, 0, 0,  0, 0, 0, 0,   0, 1, 0, 0,  0, 0, 1, 0],
+               [0, 0, 1, 1,  0, 0, 0, 1,   0, 0, 1, 1,  0, 0, 1, 1],
+               [0, 1, 0, 1,  1, 1, 0, 1],
+               [1, 1, 1, 1],
+               [1, 0],
+               [1]],
+
+    'Bailey': [[0, 1, 0, 0,  1, 0, 0, 0,   0, 0, 0, 0,  1, 0, 1, 0,
+                0, 1, 1, 0,  0, 0, 0, 0,   0, 0, 1, 0,  0, 0, 0, 0],
+               [0, 1, 1, 1,  0, 1, 1, 1,   0, 1, 1, 1,  0, 1, 1, 1],
+               [0, 0, 1, 0,  0, 0, 1, 1],
+               [0, 1, 0, 0],
+               [0, 1],
+               [0]],
+
+    'Bob':    [[0, 1, 0, 0,  0, 0, 0, 0,   0, 0, 0, 0,  0, 0, 0, 0,
+                0, 1, 0, 0,  0, 0, 0, 0,   0, 0, 0, 0,  1, 0, 0, 0],
+               [0, 1, 1, 1,  0, 1, 1, 1,   0, 1, 1, 1,  0, 1, 1, 1],
+               [0, 1, 1, 1,  0, 1, 1, 0],
+               [0, 0, 0, 0],
+               [0, 1],
+               [1]],
+
+    'Dave':   [[0, 1, 0, 0,  1, 0, 0, 0,   0, 1, 0, 0,  0, 0, 1, 0,
+                0, 0, 0, 0,  0, 0, 0, 0,   0, 0, 0, 0,  1, 0, 1, 0],
+               [0, 0, 0, 1,  0, 1, 1, 0,   0, 0, 1, 0,  0, 1, 1, 1],
+               [0, 1, 1, 0,  0, 0, 1, 0],
+               [0, 1, 1, 0],
+               [0, 1],
+               [0]],
+
+    'Gary':   [[0, 1, 1, 0,  0, 1, 0, 0,   0, 0, 1, 0,  0, 0, 1, 0,
+                0, 0, 0, 0,  0, 0, 0, 0,   0, 0, 1, 0,  1, 0, 1, 0],
+               [0, 1, 0, 1,  1, 1, 1, 1,   0, 1, 0, 0, 1, 1, 1, 1],
+               [0, 0, 1, 1,  0, 0, 0, 1],
+               [0, 0, 0, 0],
+               [0, 1],
+               [1]],
+
+    'Tom':    [[0, 1, 0, 0,  0, 0, 1, 0,   0, 0, 0, 0,  0, 0, 0, 0,
+                0, 1, 0, 0,  0, 0, 0, 0,   0, 0, 0, 0,  0, 0, 1, 0],
+               [0, 1, 1, 1,  0, 1, 1, 1,   0, 1, 0, 0,  0, 1, 1, 1],
+               [0, 1, 0, 0,  0, 1, 1, 0],
+               [0, 1, 0, 0],
+               [0, 0],
+               [1]],
+
+    }
+p_width = len(max(predictions, key=len))
+
+
+actual = [[0, 1, 1, 0,  1, 0, 1, 0,   0, 0, 1, 0,  0, 0, 0, 0,
+           0, 0, 1, 0,  0, 0, 0, 0,   0, 0, 0, 0,  1, 1, 0, 0],
+          [0, 1, 0, 0,  0, 1, 1, 0,   0, 1, 0, 1,  1, 1, 0, 1],
+          [0, 0, None, None, 0, 1, None, None],
+          [None, None, None, None],
+          [None, None],
+          [None]]
+
+
+for player in predictions:
+    v = predictions[player]
+    assert len(v) == 6
+    assert [len(i) for i in v] == [32, 16, 8, 4, 2, 1]
+    assert all(j in (0, 1) for i in v for j in i)
+
+
+def make_tree(pred):
+    prev = range(64)
+    tree = [[None] * 2**i for i in range(6-1, -1, -1)]
+    for col in range(len(pred)):
+        for row in range(len(pred[col])):
+            if pred[col][row] is None:
+                tree[col][row] = None
+            else:
+                tree[col][row] = prev[2 * row + pred[col][row]]
+        prev = tree[col]
+    return tree
+
+actual_tree = make_tree(actual)
+
+
+def print_player(player):
+    if player == 'Actual':
+        tree = actual_tree
+    else:
+        tree = make_tree(predictions[player])
+    print player
+    for row in range(len(tree[0])):
+        if not row % 8:
+            print
+        for col in range(6):
+            team = tree[col][row / 2**col]
+            if team is None:
+                team = '???'
+            else:
+                team = teams[team]
+            print '%-*s' % (width, team),
+            if row & 2**col:
+                break
+        print
+    print
+
+
+def score_range(player):
+    p_tree = make_tree(predictions[player])
+    min_score = 0
+    ns = len(scores)
+    max_score = sum(2**(ns - i - 1) * scores[i] for i in range(ns))
+    for col in range(len(p_tree)):
+        for row in range(len(p_tree[col])):
+            if p_tree[col][row] == actual_tree[col][row]:
+                min_score += scores[col]
+            else:
+                a = actual_tree[col][row]
+                if a is not None:
+                    max_score -= scores[col]
+                    if col and a not in p_tree[col - 1][2*row:2*row+2]:
+                        max_score -= scores[col]
+    return (min_score, max_score)
+
+
+# Example: check that Bob's data is entered correctly.
+# print_player('Bob')
+
+
+for player in sorted(predictions):
+    p_colon = player + ':'
+    print '%-*s %-3s %-3s' % ((p_width + 1, p_colon) + score_range(player))
